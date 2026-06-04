05afeeaba3ed39a0bcb7ede7d07a1fabcf846d60
Leandro Araque <lean.araque@gmail.com>
2026-06-03 22:00:38 -0300

    Update code_class3.2.js

--- Semana 3 Multi-modalidad/Clase 6/code_class3.2.js
+++ Semana 3 Multi-modalidad/Clase 6/code_class3.2.js
@@ -1,4 +1,5 @@
-// Recorremos los datos y le cambiamos el nombre y extensión al archivo binario
+// Telegram entrega la nota de voz como binario sin nombre ni extension.
+// Whisper necesita un archivo con extension valida (.ogg) para procesarlo.
 for (const item of $input.all()) {
   if (item.binary && item.binary.data) {
     item.binary.data.fileName = "nota_de_voz.ogg";
